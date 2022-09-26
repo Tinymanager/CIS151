e6aeb0319a981b4752d5700263b8f06b83e916ba
Ethan Pedrick <ethan.pedrick@gmail.com>
2022-09-26 16:49:44 -0400

    Commit

---
+++ Unit_3/PedrickUnit3a/AgeValidity.java
@@ -0,0 +1,16 @@
+package Unit_3.PedrickUnit3a;
+
+import java.util.Scanner;
+
+public class AgeValidity {
+
+    public static void main(String[] args) {
+        System.out.println("Enter age");
+        Scanner keyboard = new Scanner(System.in);
+        int age = keyboard.nextInt();
+        boolean drivingUnderAge = false;
+        drivingUnderAge = age <= 18;
+
+        System.out.println("Driving Under Age " + drivingUnderAge);
+    }
+}
