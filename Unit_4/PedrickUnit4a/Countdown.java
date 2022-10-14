6fd71662f83bb631dfb10de3417580fa554f5499
Ethan Pedrick <ethan.pedrick@gmail.com>
2022-10-14 15:11:55 -0400

    commit

---
+++ Unit_4/PedrickUnit4a/Countdown.java
@@ -0,0 +1,24 @@
+package Unit_4.PedrickUnit4a;
+
+public class Countdown {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Countdown to Launch: ");
+
+        for(int i = 0; i <= 5; i++) {
+            System.out.print(i +" "); 
+        }
+
+        System.out.println("Blast Off!");
+
+        
+    System.out.println("Let's Count Even Numbers: ");
+
+    for(int i = 0; i <= 20; i = i + 2) {
+        System.out.print(i +" "); 
+    }
+
+    System.out.println("Done!");
+}
+}
