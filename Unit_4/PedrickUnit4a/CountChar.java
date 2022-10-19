9ee5f7c4aee9a535ba55075a5f7d2925ae960faf
Ethan Pedrick <ethan.pedrick@gmail.com>
2022-10-19 15:49:14 -0400

    commit

---
+++ Unit_4/PedrickUnit4a/CountChar.java
@@ -0,0 +1,26 @@
+package Unit_4.PedrickUnit4a;
+
+public class CountChar {
+
+    public static void main(String[] args) {
+
+        String str = "www.oracle.com";
+
+        int max = str.length();
+        int count = 0;
+        for (int i = 0; i < max; i++){
+            
+            if (str.charAt(i) != 'w') {
+                continue;
+            } 
+            count++;
+        }
+        
+        
+        System.out.println("Counting w : " + count );
+        //is i < max, if string character isnt = w continue and increase i
+        // if (str.charAt(i) != 'w')
+        //                continue;
+
+    }
+}
