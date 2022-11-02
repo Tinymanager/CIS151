ec223cbd81908735c3aca4b2e1590b4142eebb15
Ethan Pedrick <ethan.pedrick@gmail.com>
2022-11-02 15:24:51 -0400

    c

--- Unit_5/PedrickPrisonerDilemma/PrisonTest.java
+++ Unit_5/PedrickPrisonerDilemma/PrisonTest.java
@@ -19,5 +19,32 @@ public class PrisonTest {
         System.out.println(bob == sally);
         System.out.println(bob);
         System.out.println(sally);
+
+        bob = sally;
+        System.out.println(bob == sally);
+        System.out.println(bob);
+        System.out.println(sally);
+
+        Prisoner bob2 = new Prisoner();
+        bob2.name = "bob";
+        bob2.height = 2.08;
+        bob2.sentence = 4;
+
+        Prisoner sally2 = new Prisoner();
+        sally2.name = "Sally";
+        sally2.height = 1.73;
+        sally2.sentence = 3;
+
+        System.out.println(bob2 == sally2);
+
+        System.out.println(sally2.name);
+        sally2.name = "Dennis";
+        System.out.println(sally2.name);
+
+        String s1 = new String("Test");
+        String s2 = new String("Test");
+        System.out.println(s1);
+        System.out.println(s2);
+
     } 
 }
