e9c78f5e4750f10dea6b310903560a6516539725
Ethan Pedrick <ethan.pedrick@gmail.com>
2022-11-18 19:25:23 -0500

    c

--- Unit_6/PedrickconcertSeries/PedrickConcert.java
+++ Unit_6/PedrickconcertSeries/PedrickConcert.java
@@ -3,42 +3,25 @@ package Unit_6.PedrickconcertSeries;
 public class PedrickConcert {
     //Fiels
     public String artist;
-    public String genre;
     public String location;
-    public String date;
-    public String venue;
-    public double capacity;
+    public int totalTickets;
 
     //Constructor
-    public PedrickConcert(String location, String date, double capacity){
-        this("Swift", "Pop Country", location, date, capacity);
-    }
-    public PedrickConcert(String artist, String genre, String location, String date, double capacity){
+    public PedrickConcert(String artist, String location, int totalTickets){
         this.artist = artist;
-        this.genre = genre;
         this.location = location;
-        this.date = date;
-        this.capacity = capacity;
+        this.totalTickets = totalTickets;
     }
     
 
     //Methods
-    public void announcement(){
-        System.out.println(artist + " " + genre + " is coming to " + location + " on " + date + ", the venue will hold " + capacity + " fans.");
-    }
-
-    public void ticketPrint(){
-        System.out.println("Artist: "+artist);
-        System.out.println("Location: "+location);
-        System.out.println("Date: "+date);
-    }
-    public void ticketPrint(Boolean truefalse){
-        if (truefalse == true){
-            System.out.println("Artist: "+artist);
-            System.out.println("Location: "+location);
-            System.out.println("Date: "+date);
-            announcement();
+    public boolean ticketsAvailable(int num) {
+        if (num <= totalTickets){
+            return true;
         }
+        else if (num > totalTickets){
+            return false;
+        } else {return false;}
     }
 
 }
--- Unit_6/PedrickconcertSeries/concertTest.java
+++ Unit_6/PedrickconcertSeries/concertTest.java
@@ -1,11 +1,46 @@
 package Unit_6.PedrickconcertSeries;
 
+import java.util.Scanner;
+
 public class concertTest {
-    public static void main(String[]args){
-        PedrickConcert nashville = new PedrickConcert("Swift", "Pop Country","Nashville", "0506", 200000);
-
-        nashville.ticketPrint();
-        nashville.ticketPrint(true);
-
+	public static void main(String[] args) {
+		
+    PedrickConcert summerTrip = new PedrickConcert("Swift", "Nashville", 100);
+    PedrickConcert summerTrip2 = new PedrickConcert("Rhett", "Atlanta", 50);
+    PedrickConcert summerTrip3 = new PedrickConcert("Brown", "Cleveland", 20);
+	//Example Concert summerTrip = new Concert("Swift","Nashville",100);
+		
+		
+	//Output statements to get user input have been provided below:
+    System.out.println("What artist do you want to see:?");
+    Scanner keyboard = new Scanner(System.in);
+    String artist = keyboard.nextLine();
+    System.out.println("Which venue are you interested in?");
+    String location = keyboard.nextLine();
+    //I am giving you the integer.parseInt method.  This method is used when you have a string that is a number format it converts it to a int
+    System.out.println("How many tickets do you need?");
+    int numTickets = Integer.parseInt(keyboard.nextLine());
+    //declare a Boolean variable that will hold the return value of the ticketsAvailable() method
+    Boolean ticketcheck;
+	//giving you the .equal method again, we used last week in PrisonersDilemma project
+    
+    if(artist.equals(summerTrip.artist) && location.equals(summerTrip.location)) {
+    	//use the Boolean variable to run the TicketsAvailable and hold the return value
+        ticketcheck = summerTrip.ticketsAvailable(numTickets);
+    } else if (artist.equals(summerTrip2.artist) && location.equals(summerTrip2.location)) {
+        ticketcheck = summerTrip2.ticketsAvailable(numTickets);
+    } else if (artist.equals(summerTrip3.artist) && location.equals(summerTrip3.location)) {
+        ticketcheck = summerTrip3.ticketsAvailable(numTickets);
     }
+        
+	//one more if/else that will run if Boolean variable created above is true.  Outprint statement have been provided below:
+    if (ticketcheck = true){
+        System.out.println(numTickets+ " tickets are available for " + summerTrip.artist+ " at " + summerTrip.location+ ".");
+    } else {
+        System.out.println("No tickets available are available for " + summerTrip.artist+ " at " + summerTrip.location+ ".");
+    }
+
+}
+        
+
 }
